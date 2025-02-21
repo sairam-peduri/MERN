e2b1785783c5028180358b87baf25d95ac554619
sairam-peduri <22b81a67c5@cvr.ac.in>
2025-02-21 12:19:39 +0530

    coe commit

--- zomato/src/pages/Login.jsx
+++ zomato/src/pages/Login.jsx
@@ -4,6 +4,13 @@ const Login = () => {
   return (
     <div>
       Login Page
+      <form>
+        <label name="username">Username :</label>
+        <input type="text" id="u"name="user"></input>
+        <label name="password">Password :</label>
+        <input type="password" id="p"name="pass"></input>
+        <input type="submit">submit</input>
+      </form>
     </div>
   )
 }
