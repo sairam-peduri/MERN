a0f509b67d37033a0e42f8898dbf253f2c6b075f
sairam-peduri <22b81a67c5@cvr.ac.in>
2025-02-21 12:27:25 +0530

    coe commit

--- zomato/src/App.jsx
+++ zomato/src/App.jsx
@@ -1,15 +1,11 @@
 import React from 'react'
 import './App.css'
-import Footer from './components/Footer'
-import Navbar from './components/Navbar'
 import Home from './pages/Home'
 
 const App = () => {
   return (
     <div>
-      <Navbar/>
       <Home/>
-      <Footer/>
     </div>
   )
 }
